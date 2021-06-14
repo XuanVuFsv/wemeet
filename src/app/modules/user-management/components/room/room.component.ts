a83863216b5919e44fb97e62f318eb89ad852d59
letuankhoi.dev <letuankhoi.dev@gmail.com>
2021-06-14 07:39:48 +0700

    update room mng

--- src/app/modules/user-management/components/room/room.component.ts
+++ src/app/modules/user-management/components/room/room.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomComponent implements OnInit {
 
+  public listOfData;
+
+  public room = [
+    {
+      id: 1,
+      name: 'Quản trị viên',
+    },
+    {
+      id: 1,
+      name: 'Trưởng phòng',
+    },
+    {
+      id: 1,
+      name: 'Nhân viên',
+    },
+  ];
+
+  public teams = [
+    {
+      id: 1,
+      name: 'Rocket Task',
+    },
+    {
+      id: 1,
+      name: 'Rash Me',
+    },
+    {
+      id: 1,
+      name: 'Buble Boll',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
