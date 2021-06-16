b1d705c106e1aa16b899ddc8ee4e644fd2ed503d
letuankhoi.dev <letuankhoi.dev@gmail.com>
2021-06-16 10:00:37 +0700

    add new employee modals

---
+++ src/app/modules/user-management/modals/new-employee/new-employee.component.ts
@@ -0,0 +1,92 @@
+import { FormBuilder, FormControl } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+import { Observable, Observer } from 'rxjs';
+
+@Component({
+  selector: 'app-new-employee',
+  templateUrl: './new-employee.component.html',
+  styleUrls: ['./new-employee.component.scss']
+})
+export class NewEmployeeComponent implements OnInit {
+  @Input() isVisible: boolean;
+  @Output() modalEvent = new EventEmitter<string>();
+  public newEmployeeForm: any;
+  public loading = false;
+  public avatarUrl?: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private msg: NzMessageService
+  ) {
+    this.newEmployeeForm = this.formBuilder.group({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      nickname: new FormControl(null),
+      position: new FormControl(null),
+      role: new FormControl(null)
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  public beforeUpload = (file: NzUploadFile, _fileList: NzUploadFile[]) => {
+    return new Observable((observer: Observer<boolean>) => {
+      const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+      if (!isJpgOrPng) {
+        this.msg.error('You can only upload JPG file!');
+        observer.complete();
+        return;
+      }
+      const isLt2M = file.size! / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.msg.error('Image must smaller than 2MB!');
+        observer.complete();
+        return;
+      }
+      observer.next(isJpgOrPng && isLt2M);
+      observer.complete();
+    });
+  };
+
+  private getBase64(img: File, callback: (img: string) => void): void {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result!.toString()));
+    reader.readAsDataURL(img);
+  }
+
+  public onChange(info: { file: NzUploadFile }): void {
+    switch (info.file.status) {
+      case 'uploading':
+        this.loading = true;
+        break;
+      case 'done':
+        // Get this url from response in real world.
+        this.getBase64(info.file!.originFileObj!, (img: string) => {
+          this.loading = false;
+          this.avatarUrl = img;
+        });
+        break;
+      case 'error':
+        this.msg.error('Network error');
+        this.loading = false;
+        break;
+    }
+  }
+
+
+  public onRemove(): void {
+
+  }
+
+  public onClose(): void {
+    this.modalEvent.emit('true');
+  }
+
+  public onCreate(): void {
+
+  }
+
+
+}
