3aeecd805a3e7e2a8bce3e30c9e485cc324aee7c
letuankhoi.dev <letuankhoi.dev@gmail.com>
2021-06-21 09:09:24 +0700

    udpate code

--- src/app/modules/user-management/user-management.module.ts
+++ src/app/modules/user-management/user-management.module.ts
@@ -1,11 +1,25 @@
+import { MeetingModule } from './components/meeting/meeting.module';
+import { RoomModule } from './components/room/room.module';
+import { UserModule } from './components/user/user.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserManagementComponent } from './user-management.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { UserManagementRoutingModule } from './user-management.routing';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [UserManagementComponent],
-  imports: [CommonModule, SharedModule, UserManagementRoutingModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    NzTabsModule,
+    NzIconModule,
+    UserManagementRoutingModule,
+    UserModule,
+    RoomModule,
+    MeetingModule
+  ]
 })
 export class UserManagementModule {}
